bb108a4120674e3bf5e7c3b36d7a394bd6f675f4
Robert Bullfrog <williampikus@gmail.com>
2022-03-21 16:52:54 -0700

    Models created

---
+++ models/Item.js
@@ -0,0 +1,56 @@
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/connection');
+
+class Item extends Model {}
+
+Item.init(
+    {
+      id: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        primaryKey: true,
+        autoIncrement: true,
+      },
+      name: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      type: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      desc: {
+        type: DataTypes.STRING,
+        allowNull: true,
+      },
+      quality: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      user_id: {
+        type: DataTypes.INTEGER,
+        allowNull: true,
+        references: {
+            model: 'user',
+            key: 'id',
+          },
+      },
+      cat_id: {
+        type: DataTypes.INTEGER,
+        references: {
+          model: 'category',
+          key: 'id',
+        },
+      },
+    },
+    {
+      sequelize,
+      timestamps: false,
+      freezeTableName: true,
+      underscored: true,
+      modelName: 'item',
+    }
+  );
+  
+  module.exports = Item;
+  
---
+++ models/index.js
@@ -0,0 +1,29 @@
+const User = require('./User');
+const Item = require('./Item');
+const Category = require('./Category');
+const Community = require('./Community');
+
+User.hasMany(Item, {
+  foreignKey: 'user_id',
+  onDelete: 'CASCADE'
+});
+
+Item.belongsTo(User, {
+  foreignKey: 'user_id'
+});
+
+Category.hasMany(Item, {
+  foreignKey: 'cat_id'
+});
+
+Item.belongsTo(Category, {
+    foreignKey: 'cat_id'
+  });
+
+
+
+
+
+
+
+module.exports = { User, Item, Category, Community };
