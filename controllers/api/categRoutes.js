86e010d8c92fc3ea57cecd4ab3562d14bfea2e9f
CodeNinja <inicholas8686@gmail.com>
2022-03-23 19:48:23 -0700

    feat(routes):categRoutes.js updated to run

--- controllers/api/categRoutes.js
+++ controllers/api/categRoutes.js
@@ -1,57 +1,71 @@
 const router = require('express').Router();
 const { Item, Category } = require('../../models');
 
-// GET one category
-router.get('/category/:id', async (req, res) => {
+// GET all categories
+router.get('/', async (req, res) => {
     // If the user is not logged in, redirect the user to the login page
-    if (!req.session.loggedIn) {
-      res.redirect('/login');
-    } else {
+    // if (!req.session.loggedIn) {
+    //   res.redirect('/login');
+    // } else {
       // If the user is logged in, allow them to view the category chosen
       try {
-        const dbCategoryData = await Category.findByPk(req.params.id, {
-          include: [
-            {
-              model: Item,
-              attributes: [
-                'id',
-                'name',
-                'desc',
-                'quality',
-                'user_id',
-                'cat_id',
-              ],
-            },
-          ],
+        const dbCategoryData = await Category.findAll(req.params.id, {
+          // include: [
+          //   {
+          //     model: Item,
+          //     attributes: [
+          //       'name',
+          //       'desc',
+          //       'quality',
+          //     ],
+          //   },
+          // ],
         });
-        const category = dbCategoryData.get({ plain: true });
-        res.render('category', { category, loggedIn: req.session.loggedIn });
+        // const category = dbCategoryData.get({ plain: true });
+        // res.render('category', { category, loggedIn: req.session.loggedIn });
+        res.json(dbCategoryData);
       } catch (err) {
         console.log(err);
         res.status(500).json(err);
       }
-    }
+    // }
+  });
+
+  // GET cats by ID
+  router.get('/:id', (req, res) => {
+    // find one category by its `id` value
+    // be sure to include its associated Products
+    Category.findOne({
+      // include: {
+      //   model: Product,
+      // },
+      where: {
+        id: req.params.id,
+      },
+    }).then((catData) => {
+      res.json(catData);
+    });
   });
   
-  // GET one item
-  router.get('/item/:id', async (req, res) => {
-    // If the user is not logged in, redirect the user to the login page
-    if (!req.session.loggedIn) {
-      res.redirect('/login');
-    } else {
-      // If the user is logged in, allow them to view the item
-      try {
-        const dbItemData = await Item.findByPk(req.params.id);
+  // save for itemsRoutes
+  // router.get('/item/:id', async (req, res) => {
+  //   // If the user is not logged in, redirect the user to the login page
+  //   if (!req.session.loggedIn) {
+  //     res.redirect('/login');
+  //   } else {
+  //     // If the user is logged in, allow them to view the item
+  //     try {
+  //       const dbItemData = await Item.findByPk(req.params.id);
   
-        const item = dbItemData.get({ plain: true });
+  //       const item = dbItemData.get({ plain: true });
   
-        res.render('painting', { item, loggedIn: req.session.loggedIn });
-      } catch (err) {
-        console.log(err);
-        res.status(500).json(err);
-      }
-    }
-  });
+  //       res.render('painting', { item, loggedIn: req.session.loggedIn });
+  //     } catch (err) {
+  //       console.log(err);
+  //       res.status(500).json(err);
+  //     }
+  //   }
+  // });
   
   router.get('/login', (req, res) => {
     if (req.session.loggedIn) {
