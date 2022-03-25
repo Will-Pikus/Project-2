003e9a8a51b26638e6a0e9a4691faca1fcd05a3f
CodeNinja <inicholas8686@gmail.com>
2022-03-24 19:29:44 -0700

    several changes

--- controllers/api/categRoutes.js
+++ controllers/api/categRoutes.js
@@ -36,14 +36,11 @@ router.get('/', async (req, res) => {
     // find one category by its `id` value
     // be sure to include its associated Products
     try {
-      const dbCategoryData = await Category.findOne(req.params.id, {
+      const dbCategoryData = await Category.findByPk(req.params.id, {
         // include: [
         //   {
-        //     model: Item,
         //     attributes: [
         //       'name',
-        //       'desc',
-        //       'quality',
         //     ],
         //   },
         // ],
@@ -86,6 +83,16 @@ router.get('/', async (req, res) => {
   //     }
   //   }
   // });
+
+  // CREATE a category
+router.post('/', async (req, res) => {
+  try {
+    const categoryData = await Category.create(req.body);
+    res.status(200).json(categoryData);
+  } catch (err) {
+    res.status(400).json(err);
+  }
+});
   
   router.get('/login', (req, res) => {
     if (req.session.loggedIn) {
--- controllers/api/index.js
+++ controllers/api/index.js
@@ -2,9 +2,11 @@ const router = require('express').Router();
 const userRoutes = require('./userRoutes');
 const categRoutes = require('./categRoutes');
 const itemRoutes = require('./itemRoutes');
+const communityRoutes = require('./communityRoutes');
 
 router.use('/users', userRoutes);
 router.use('/categories', categRoutes);
 router.use('/items', itemRoutes);
+router.use('/communities', communityRoutes);
 
 module.exports = router;
