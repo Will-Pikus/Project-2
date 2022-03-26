1fcbf389542b879e3b27916cc9a5eeb3006af669
CodeNinja <inicholas8686@gmail.com>
2022-03-26 09:22:50 -0700

    added a couple handlebars, js files, and updated routes

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -1,77 +1,77 @@
-const router = require('express').Router();
-const { Category, Community, Item, User } = require('../models');
-const withAuth = require('../utils/auth');
+const router = require("express").Router();
+const { Category, Community, Item, User } = require("../models");
+const { sequelize } = require("../models/User");
+const withAuth = require("../utils/auth");
 
-var randomItem = Item[Math.floor(Math.random(),Item.length)];
+var randomItem = Item[Math.floor(Math.random(), Item.length)];
 
-router.get('/', async (req, res) => {
+router.get("/", async (req, res) => {
   try {
-    const itemData = await Item.findAll({
-      include: [
-        {
-          model: User,
-          attributes: ['username'],
-        },
-      ],
-    });
-    const items = itemData.map((itemData) => itemData.get({ plain: true }));
-    res.render('homepage', { 
-      items, 
-      logged_in: req.session.logged_in 
-    });
+    const itemData = await Item.findAll({});
+    res.json(itemData);
   } catch (err) {
     res.status(500).json(err);
   }
 });
 
-router.get('/items/:id', async (req, res) => {
+router.get("/:id", async (req, res) => {
   try {
     const itemData = await Item.findByPk(req.params.id, {
       include: [
         {
           model: User,
-          attributes: ['username'],
+          attributes: ["username"],
         },
       ],
     });
 
-    const items = itemData.get({ plain: true });
-
-    res.render('item', {
-      ...items,
-      logged_in: req.session.logged_in
-    });
+    // const items = itemData.get({ plain: true });
+    res.json(itemData);
+    // res.render('item', {
+    //   ...items,
+    //   logged_in: req.session.logged_in
+    // });
   } catch (err) {
     res.status(500).json(err);
   }
 });
 
-router.get('/profile', withAuth, async (req, res) => {
-  try {
-    const userData = await User.findByPk(req.session.user_id, {
-      attributes: { exclude: ['password'] },
-      include: [{ model: Project }],
-    });
+// router.get('/cats', async (req, res) => {
+//   try {
+//     const categoryData = await Category.findAll({
 
-    const user = userData.get({ plain: true });
+//     });
 
-    res.render('profile', {
-      ...user,
-      logged_in: true
-    });
-  } catch (err) {
-    res.status(500).json(err);
-  }
-});
+//     // const user = userData.get({ plain: true });
 
-router.get('/login', (req, res) => {
+//     res.json(categoryData)
+//   } catch (err) {
+//     res.status(500).json(err);
+//   }
+// });
+
+// router.get('/comms', async (req, res) => {
+//   try {
+//     const communityData = await Community.findAll({
+
+//     });
+
+//     // const user = userData.get({ plain: true });
+
+//     res.json(communityData)
+//   } catch (err) {
+//     res.status(500).json(err);
+//   }
+// });
+
+router.get("/login", (req, res) => {
   // If the user is already logged in, redirect the request to another route
   if (req.session.logged_in) {
-    res.redirect('/profile');
+    res.redirect("/profile");
     return;
   }
 
-  res.render('login');
+  res.render("login");
 });
 
 module.exports = router;
