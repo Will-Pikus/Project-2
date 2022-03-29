f461d0e00c813e53527640cb856e0962326f3a22
Robert Bullfrog <williampikus@gmail.com>
2022-03-28 19:51:21 -0700

    Heroku push - testing connection

--- config/connection.js
+++ config/connection.js
@@ -5,11 +5,15 @@ const Sequelize = require('sequelize');
 const sequelize = process.env.JAWSDB_URL
   ? new Sequelize(process.env.JAWSDB_URL)
   : new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
-      host: 'localhost',
+      host: process.env.MYSQL_HOST,
       dialect: 'mysql',
       dialectOptions: {
         decimalNumbers: true,
       },
+      production: {
+        use_env_variable: JAWSDB_URL,
+        dialect: 'mysql'
+      }
     });
 
 module.exports = sequelize;
